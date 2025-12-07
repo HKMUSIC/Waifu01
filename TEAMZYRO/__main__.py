1332a7192e00d39be56b24f138dcadd6f6fd4a95
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-07 10:07:27 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -9,14 +9,13 @@ def main() -> None:
         imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
     LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
-    await ZYRO.start()
-
+    ZYRO.start()
     application.run_polling(drop_pending_updates=True)
     send_start_message()
     LOGGER("TEAMZYRO").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
 
-
 if __name__ == "__main__":
     main()
+    
