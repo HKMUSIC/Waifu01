74ee2b6ec8f2e3876b0d2ef9fd9fd166182e948b
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-07 10:05:45 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -2,37 +2,21 @@ from TEAMZYRO import *
 import importlib
 import logging
 from TEAMZYRO.modules import ALL_MODULES
-from pyrogram import idle
-import asyncio
 
 
-# -------------------------------------------------
-# LOAD ALL MODULES
-# -------------------------------------------------
-def load_modules():
+def main() -> None:
     for module_name in ALL_MODULES:
-        importlib.import_module("TEAMZYRO.modules." + module_name)
-    LOGGER("TEAMZYRO.modules").info("All Modules Loaded Successfully.")
-
-
-# -------------------------------------------------
-# ASYNC MAIN FUNCTION
-# -------------------------------------------------
-async def start_bot():
-    load_modules()
+        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
+    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
     await ZYRO.start()
-    print("Bot Started Successfully!")
 
+    application.run_polling(drop_pending_updates=True)
+    send_start_message()
     LOGGER("TEAMZYRO").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
 
-    await idle()   # keep bot running
 
-
-# -------------------------------------------------
-# RUN ASYNCIO LOOP
-# -------------------------------------------------
 if __name__ == "__main__":
-    asyncio.run(start_bot())
+    main()
