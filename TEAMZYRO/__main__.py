af82a900a5acd20be3601195cd14d9810cf9240f
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-07 17:06:05 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,41 +1,21 @@
-# TEAMZYRO/main.py
-import asyncio
-import importlib
 from TEAMZYRO import *
+import importlib
+import logging
 from TEAMZYRO.modules import ALL_MODULES
 
 
-async def start_bot():
-    # Load modules
+def main() -> None:
     for module_name in ALL_MODULES:
-        importlib.import_module(f"TEAMZYRO.modules.{module_name}")
-
+        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
     LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
-    # Start Pyrogram
-    await ZYRO.start()
-    LOGGER("TEAMZYRO").info("Pyrogram (ZYRO) started.")
-
-    # Start PTB
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling(drop_pending_updates=True)
-    LOGGER("TEAMZYRO").info("PTB Polling started.")
-
-    # ---------------------------
-    # NO send_start_message here
-    # ---------------------------
-
+    ZYRO.start()
+    application.run_polling(drop_pending_updates=True)
+    send_start_message()
     LOGGER("TEAMZYRO").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
 
-    await asyncio.Event().wait()
-
-
-def main():
-    asyncio.run(start_bot())
-
-
 if __name__ == "__main__":
     main()
+    
