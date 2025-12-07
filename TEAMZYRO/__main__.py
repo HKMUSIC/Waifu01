0d38231e1de8ba3b7ab3aafee8a10d25513cdfd4
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-07 10:01:50 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -5,18 +5,18 @@ from TEAMZYRO.modules import ALL_MODULES
 
 
 def main() -> None:
-for module_name in ALL_MODULES:
-imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
-LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    for module_name in ALL_MODULES:
+        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
+    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+
+    await ZYRO.start()
+
+    application.run_polling(drop_pending_updates=True)
+    send_start_message()
+    LOGGER("TEAMZYRO").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
 
 
-await ZYRO.start()
-
-application.run_polling(drop_pending_updates=True)  
-send_start_message()  
-LOGGER("TEAMZYRO").info(  
-    "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"  
-)
-
-if name == "main":
-main()
+if __name__ == "__main__":
+    main()
