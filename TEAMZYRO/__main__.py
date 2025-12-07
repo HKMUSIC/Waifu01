aa11beea3df24f2e3129e7debff1c8383fb85c9a
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-07 09:55:42 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -2,6 +2,8 @@ from TEAMZYRO import *
 import importlib
 import logging
 from TEAMZYRO.modules import ALL_MODULES
+from pyrogram import idle
+import asyncio
 
 
 def main() -> None:
@@ -9,7 +11,14 @@ def main() -> None:
         imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
     LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
-    ZYRO.start()
+
+async def main():
+    await ZYRO.start()
+    print("Bot started!")
+    await idle()   # keeps event loop alive safely
+
+asyncio.run(main())
+
     application.run_polling(drop_pending_updates=True)
     send_start_message()
     LOGGER("TEAMZYRO").info(
