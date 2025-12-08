ad351f3c39f99d4d54a8d3b47e88f6be2f52ef31
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-08 12:47:19 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,21 +1,28 @@
-from TEAMZYRO import *
+import asyncio
 import importlib
-import logging
+from TEAMZYRO import *
 from TEAMZYRO.modules import ALL_MODULES
+import logging
 
 
-def main() -> None:
+async def start_all():
     for module_name in ALL_MODULES:
-        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
-    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        importlib.import_module("TEAMZYRO.modules." + module_name)
 
-    ZYRO.start()
-    application.run_polling(drop_pending_updates=True)
-    send_start_message()
+    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 🥳")
+    
+    await ZYRO.start()
+    asyncio.create_task(application.run_polling(drop_pending_updates=True))
+    await send_start_message()
+
     LOGGER("TEAMZYRO").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY GOJOXNETWORK☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
 
+
+def main():
+    asyncio.run(start_all())
+
+
 if __name__ == "__main__":
     main()
-    
